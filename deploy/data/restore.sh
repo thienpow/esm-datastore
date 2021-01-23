4dbd02b2c398e6ed4457a3bfafb7932c70ac8066
Ken Chong <thienpow@gmail.com>
2021-01-23 13:52:36 +0800

    minor

--- deploy/data/restore.sh
+++ deploy/data/restore.sh
@@ -1,5 +1,6 @@
-
-psql -c "DROP DATABASE esmstore;" -d postgres
-psql -c "CREATE DATABASE esmstore;" -d postgres
-psql -f initdb.sql -d esmstore
-psql -f preset.sql -d esmstore
+#!/bin/sh
+export PGPASSWORD=notezpass99~-~
+psql -h localhost -p 5432 -U postgres -c "DROP DATABASE esmstore;" -d postgres
+psql -h localhost -p 5432 -U postgres -c "CREATE DATABASE esmstore;" -d postgres
+psql -h localhost -p 5432 -U postgres -f initdb.sql -d esmstore
+psql -h localhost -p 5432 -U postgres -f preset.sql -d esmstore
