3b595c069c7e202695c8f27b30b71dd8817510fb
Ken Chong <thienpow@gmail.com>
2021-04-02 17:14:48 +0800

    prize.tickets_collected <= prize.tickets_required

--- src/svc/esmapi.rs
+++ src/svc/esmapi.rs
@@ -980,13 +980,26 @@ impl esmapi_proto::esm_api_server::EsmApi for EsmApiServer {
         group_id: prize.group_id
       };
       
+      //type_id 1=Featured, 2=Premium
       if prize.type_id == 1 || prize.type_id == 2 {
-        result.push(li);
+        
+        //For Featured and Premium prize, need to check if the tickets_collected is smaller than tickets_required, then only allow to list
+        if prize.tickets_collected <= prize.tickets_required {
+          result.push(li);
+        }
+
+      //type_id 3=Time Sensitive, 4=Automated Entry
       } else if prize.type_id == 3 || prize.type_id == 4 {
+
+        //if it's Prize 3 or 4, we need to check if now is greater than or equal to scheduled_on, meaning it's started.
         if scheduled_on <= adjusted_now {
+
+          // if is_repeat, meaning need to always show, because it never end.
           if prize.is_repeat {
             result.push(li);
           } else {
+
+            // if not repeat, then we need to check if it's still within the duration.
             if scheduled_off >= adjusted_now {
               result.push(li);
             }
