7b751ddb680420412d57e6beffd8b139ed02a97d
Ken Chong <thienpow@gmail.com>
2021-04-13 14:27:20 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 git pull
+cargo build --bin esmserver --release
+
 cd deploy/
 docker-compose down
 rm service/esmserver
+
 cd ..
-cargo build --bin esmserver --release
 cp target/release/esmserver deploy/service/esmserver
+
 cd deploy/
 docker-compose up --build -d
