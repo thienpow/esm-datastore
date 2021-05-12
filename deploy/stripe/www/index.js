05200f5eb1afa96f1f52e21116bdfd2e31745426
Ken Chong <thienpow@gmail.com>
2021-05-12 17:32:01 +0800

    stripe

---
+++ deploy/stripe/www/index.js
@@ -0,0 +1,9 @@
+const winston = require("winston");
+const express = require("express");
+const app = express();
+
+// require("./startup/logging")();
+require("./startup/routes")(app);
+const port = process.env.PORT || 4242;
+app.listen(port, () => winston.info(`Listening on port ${port}`));
+// app.listen(port, () => console.log(`Listening on port ${port}`));
