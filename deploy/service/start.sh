992d78afddf6867b9a0db78d152ef9d7cdb098b7
Ken Chong <thienpow@gmail.com>
2021-05-19 12:37:21 +0800

    test

--- deploy/service/start.sh
+++ deploy/service/start.sh
@@ -9,6 +9,6 @@ export BEFORE_LOGIN_TOKEN="Bearer before-login-token"
 export SERVER_TIMEZONE="8"
 
 
-# need to have at least one looping
+# IMPORTANT! need to have at least one looping... do not put & at the last command
 # /usr/local/bin/checker & 
 /usr/local/bin/esmserver
