9e73c12fb833d504f5b543556915b22e763c127f
Ken Chong <thienpow@gmail.com>
2021-05-19 09:48:35 +0800

    fix

--- deploy/stripe/start.sh
+++ deploy/stripe/start.sh
@@ -3,5 +3,6 @@
 export STRIPE_SECRET="sk_test_51IOa7gBGCaV2oEVpEDw9fOn1fUn8IaPOemu7Yjkg6N75C3ahsd7sEQr3ait4JYU55wfCBbXCRPIhcUxGMVtQYA6A003YKMqkYF"
 export DB_CONN_STRING="postgresql://postgres:notezpass99~-~@10.104.0.2:5432/esmstore"
 export SERVER_TIMEZONE="8"
+export PORT="4242"
 
-node index.js
+node /www/index.js
