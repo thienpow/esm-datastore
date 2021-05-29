d739a6ed458250657148a70108f170d1db1ada2e
Ken Chong <thienpow@gmail.com>
2021-05-29 17:12:33 +0800

    fix

--- restart_esm_admin.sh
+++ restart_esm_admin.sh
@@ -1,7 +1,14 @@
+# pull latest repo
+cd ~/esm-datastore/
 git pull
 
+# rebuild and restart the esm-admin
 cd ~/esm-datastore/deploy/
 docker-compose up -d --build esm-admin
 
 # clean  up unused docker images
 docker images -q | xargs docker rmi
+
+# show the running containers and server uptime
+docker ps -a
+uptime
