00c370511a0631d41c4c3c636f88d18af65046db
Ken Chong <thienpow@gmail.com>
2021-05-30 19:36:52 +0800

    deploy

--- deploy/service/start.sh
+++ deploy/service/start.sh
@@ -4,7 +4,7 @@ export JWK_URL="https://www.googleapis.com/service_accounts/v1/jwk/securetoken@s
 export JWK_AUDIENCE="esports-mini"
 export JWK_ISSUER="https://securetoken.google.com/esports-mini"
 export DB_CERT_PATH="/usr/local/bin/db_cert.cer"
-export DB_CONN_STRING="postgresql://doadmin:tf45h3hpz6xstby6@private-db-postgresql-dev-sgp1-32064-do-user-7964287-0.b.db.ondigitalocean.com:25061/defaultdb?sslmode=require"
+export DB_CONN_STRING="postgresql://doadmin:tf45h3hpz6xstby6@private-db-postgresql-dev-sgp1-32064-do-user-7964287-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
 export LISTEN_ON="0.0.0.0:50051"
 export BEFORE_LOGIN_TOKEN="Bearer before-login-token"
 export SERVER_TIMEZONE="8"
---
+++ src/micro_services/config.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+#[derive(Debug)]
+pub struct Configuration {
+    pub db_cert_path: String,
+    pub db_conn_string: String,
+}
+
+#[cfg(debug_assertions)]
+fn expect_env_var(name: &str, default: &str) -> String {
+    return env::var(name).unwrap_or(String::from(default));
+}
+
+#[cfg(not(debug_assertions))]
+fn expect_env_var(name: &str, _default: &str) -> String {
+    return env::var(name).expect(&format!(
+        "Environment variable {name} is not defined",
+        name = name
+    ));
+}
+
+pub fn get_configuration() -> Configuration {
+    Configuration {
+        db_cert_path: expect_env_var("DB_CERT_PATH", "cert/cert.cer"),
+        db_conn_string: expect_env_var("DB_CONN_STRING", "postgresql://doadmin:tf45h3hpz6xstby6@db-postgresql-dev-sgp1-32064-do-user-7964287-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"),
+    }
+}
