fdf1745eb89edacdc76992228a3642b2220474a9
Ken Chong <thienpow@gmail.com>
2021-05-31 02:34:03 +0800

    db conn

--- deploy/stripe/start.sh
+++ deploy/stripe/start.sh
@@ -2,7 +2,7 @@
 
 export STRIPE_SECRET="sk_test_51IOa7gBGCaV2oEVpEDw9fOn1fUn8IaPOemu7Yjkg6N75C3ahsd7sEQr3ait4JYU55wfCBbXCRPIhcUxGMVtQYA6A003YKMqkYF"
 export DB_CERT_PATH="/usr/local/bin/db_cert.cer"
-export DB_CONN_STRING="postgresql://doadmin:tf45h3hpz6xstby6@private-db-postgresql-dev-sgp1-32064-do-user-7964287-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require"
+export DB_CONN_STRING="postgresql://doadmin:tf45h3hpz6xstby6@postgres-master:5432/postgres?sslmode=require"
 export SERVER_TIMEZONE="8"
 export PORT="4242"
 
