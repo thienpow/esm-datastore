f06513e57bfd457d5c0cdf587a3f9c486f1aea98
Ken Chong <thienpow@gmail.com>
2021-05-31 02:41:14 +0800

    localhost

--- deploy/gloader/start.sh
+++ deploy/gloader/start.sh
@@ -4,7 +4,7 @@ export JWK_URL="https://www.googleapis.com/service_accounts/v1/jwk/securetoken@s
 export JWK_AUDIENCE="esports-mini"
 export JWK_ISSUER="https://securetoken.google.com/esports-mini"
 export DB_CERT_PATH="/usr/local/bin/db_cert.cer"
-export DB_CONN_STRING="postgresql://doadmin:tf45h3hpz6xstby6@postgres-master:5432/postgres?sslmode=require"
+export DB_CONN_STRING="postgresql://doadmin:tf45h3hpz6xstby6@127.0.0.1:5432/postgres?sslmode=require"
 export LISTEN_ON="0.0.0.0:3033"
 export SERVER_TIMEZONE="8"
 
