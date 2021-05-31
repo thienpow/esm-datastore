84e23878aed80b0f858653dfa2db09ee0cb8cb2f
Ken Chong <thienpow@gmail.com>
2021-05-31 17:35:31 +0800

    deploy

--- deploy/postgres/master/setup.sh
+++ deploy/postgres/master/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "host  replication          replicator         0.0.0.0/0         md5" >> "$PGDATA/pg_hba.conf"
 echo "hostssl all all all cert clientcert=verify-ca" >> "$PGDATA/pg_hba.conf"
-# psql -p 5432 -c "CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'secret'"
+
+#fill the initial data
+psql -p 5432 -f initial_data.sql -d postgres
 
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
