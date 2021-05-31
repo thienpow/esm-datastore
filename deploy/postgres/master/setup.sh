449c18ff1cd7b6514bfc386d9d45d13c61bc843f
Ken Chong <thienpow@gmail.com>
2021-05-31 17:47:36 +0800

    deploy

--- deploy/postgres/master/setup.sh
+++ deploy/postgres/master/setup.sh
@@ -4,7 +4,7 @@ echo "hostssl all all all cert clientcert=verify-ca" >> "$PGDATA/pg_hba.conf"
 
 #fill the initial data
 psql -f /tmp/initial_data.sql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"
-
+rm /tmp/initial_data.sql
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
