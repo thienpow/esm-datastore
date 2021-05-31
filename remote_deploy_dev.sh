caaa8870f8e561af32f91efa1c2053474b238bcd
Ken Chong <thienpow@gmail.com>
2021-05-31 19:39:13 +0800

    deploy

--- remote_deploy_dev.sh
+++ remote_deploy_dev.sh
@@ -127,7 +127,7 @@ case $1 in
   docker-compose up -d --build
   ;;
 88)
-  docker-compose up -d --build dbpg-master
+  docker-compose up -d --build dbpg-standby-01
   ;;
 99)
   docker-compose up --build
