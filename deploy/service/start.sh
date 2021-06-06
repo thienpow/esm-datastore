8f86d1362cae8fd832a41d98ab0b02107d7098fc
Ken Chong <thienpow@gmail.com>
2021-06-06 09:15:58 +0800

    deploy

--- deploy/service/start.sh
+++ deploy/service/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #export LISTEN_ON="0.0.0.0:50051"
-export BEFORE_LOGIN_TOKEN="Bearer before-login-token"
+#export BEFORE_LOGIN_TOKEN="Bearer before-login-token"
 export STRIPE_SECRET="sk_test_51IOa7gBGCaV2oEVpEDw9fOn1fUn8IaPOemu7Yjkg6N75C3ahsd7sEQr3ait4JYU55wfCBbXCRPIhcUxGMVtQYA6A003YKMqkYF"
 export CHECKER_TIME_WAIT="60"
 export FCM_KEY="AAAAzT8d8lM:APA91bHtFSHVIBiiSDUWSaUkrYNrcBSyhMY_JBYSeaaoWpkrxSgBO-7xgHtySH3qfTvsgHHTIsrh_SwwirCvTTuOWBl0--JhddEV8MFnEgWuGRPOXPVyTBHym5k9gh8WrCxr-rY13Y54"
